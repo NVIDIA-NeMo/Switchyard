ac31ad569a113522ae10d3deba5862a50d590e91
Greg Clark <grclark@nvidia.com>
2026-07-24 13:18:08 -0400

    feat(libsy): move stage router to libsy
    Signed-off-by: Greg Clark <grclark@nvidia.com>
    
    feat(libsy): bridge back to components for stage_router
    
    Signed-off-by: Greg Clark <grclark@nvidia.com>

--- crates/libsy/src/algorithms/util.rs
+++ crates/libsy/src/algorithms/util.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod affinity;
+pub(crate) mod stage_router;
+pub(crate) mod tool_signals;
 
 #[allow(unused_imports)]
 pub(crate) use affinity::AffinityRouter;
