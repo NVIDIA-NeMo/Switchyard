8067ad342a6904f4c39a8d4e5b7db48c03bfd041
grahamking <grahamking@users.noreply.github.com>
2026-08-03 18:58:01 +0000

    Deploy preview for PR 261 🛫

---
+++ pr-preview/pr-261/rust/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["switchyard_libsy"];
+//{"start":21,"fragment_lengths":[18]}
---
+++ pr-preview/pr-261/rust/trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["switchyard_libsy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"switchyard_libsy/enum.DriverError.html\" title=\"enum switchyard_libsy::DriverError\">DriverError</a>&gt; for <a class=\"enum\" href=\"switchyard_libsy/enum.LibsyError.html\" title=\"enum switchyard_libsy::LibsyError\">LibsyError</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;JoinError&gt; for <a class=\"enum\" href=\"switchyard_libsy/enum.LibsyError.html\" title=\"enum switchyard_libsy::LibsyError\">LibsyError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[721]}
