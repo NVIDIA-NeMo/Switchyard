4009527b169aba7f7d0603e49063a8ebc52b6903
nachiketb-nvidia <nachiketb-nvidia@users.noreply.github.com>
2026-08-03 22:23:06 +0000

    Deploy preview for PR 268 🛫

---
+++ pr-preview/pr-268/rust/trait.impl/switchyard_protocol/client/trait.Decision.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["switchyard_libsy",[["impl Decision for <a class=\"struct\" href=\"switchyard_libsy/struct.NoopDecision.html\" title=\"struct switchyard_libsy::NoopDecision\">NoopDecision</a>",0],["impl Decision for <a class=\"struct\" href=\"switchyard_libsy/struct.PassthroughDecision.html\" title=\"struct switchyard_libsy::PassthroughDecision\">PassthroughDecision</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[363]}
