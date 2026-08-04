f255d6c232adf2a8a13a89ea6cc8bc56839859c7
grahamking <grahamking@users.noreply.github.com>
2026-08-04 14:41:54 +0000

    Deploy preview for PR 269 🛫

--- pr-preview/pr-269/rust/crates.js
+++ pr-preview/pr-269/rust/crates.js
@@ -1,2 +1,2 @@
-window.ALL_CRATES = ["switchyard_libsy"];
-//{"start":21,"fragment_lengths":[18]}
+window.ALL_CRATES = ["switchyard_libsy","switchyard_protocol"];
+//{"start":21,"fragment_lengths":[18,22]}
