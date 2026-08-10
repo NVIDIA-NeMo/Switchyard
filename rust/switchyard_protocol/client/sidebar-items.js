bacb48fdf84f05c676855dbdc8a7bb67b3bd34e1
ayushag-nv <ayushag-nv@users.noreply.github.com>
2026-08-10 18:05:55 +0000

    deploy: 4fc554af55995a3479edd427d16af7c227802fa0

--- rust/switchyard_protocol/client/sidebar-items.js
+++ rust/switchyard_protocol/client/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["LlmClientError","RoutingFallbackReason"],"struct":["SimpleDecision"],"trait":["Decision","RoutedLlmClient"],"type":["BoxError"]};
+window.SIDEBAR_ITEMS = {"enum":["LlmClientError","RoutingFallbackReason"],"struct":["Decision"],"trait":["RoutedLlmClient"],"type":["BoxError"]};
